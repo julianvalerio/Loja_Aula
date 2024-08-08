c9386df0370ad9e1043e9cf7bd9f201259a9f671
julianvalerio <julianrvalerio89@gmail.com>
2024-08-08 08:56:46 -0300

    first commit

--- Loja/Loja_online/urls.py
+++ Loja/Loja_online/urls.py
@@ -3,4 +3,6 @@ from Loja_online import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("produtos/", views.listar_produtos, name="produtos"),
+    
 ]
--- Loja/Loja_online/views.py
+++ Loja/Loja_online/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from Loja_online.models import Categoria, Produto
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    num_produtos = Produto.objects.all().count()
+    num_camisas=Produto.objects.filter(categoria="1").count()    
+    context = {
+        'num_produtos': num_produtos,
+        'num_camisas': num_camisas
+    }
+    return render(request, 'index.html', context=context)
+
+def listar_produtos(request):
+    return render(request, 'produtos.html')
